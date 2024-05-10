1b91c7c069d80f8217188d71dd6c7ee11341a206
Hardik Agrawal <127330616+HardikIsACoder@users.noreply.github.com>
2024-05-10 23:53:49 +0530

    Update Question2

--- Question2.py
+++ Question2.py
@@ -1,3 +1,5 @@
+#2. Write a program to calculate the sum of series up to n term. For example, if n =5
+# the series will become 2 + 22 + 222 + 2222 + 22222 = 24690
 n = int(input("Enter n: "))
 def sum(n):
     sum = 0
